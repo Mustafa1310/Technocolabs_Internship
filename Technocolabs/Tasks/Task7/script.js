d48ef4f7dcb8a3dc5f5a2c15f866a74b3aae819e
Mustafa Malik <58935609+Mustafa1310@users.noreply.github.com>
2020-07-22 21:24:44 +0530

    Add files via upload

---
+++ Technocolabs/Tasks/Task7/script.js
@@ -0,0 +1,16 @@
+//my current age
+const myAge = 19;
+//early Years 
+let earlyYears= 2;
+earlyYears *= 10.5;
+let laterYears = myAge-2;
+laterYears *=4;
+
+console.log(laterYears);
+console.log(earlyYears);
+
+//my age in dog years
+let myAgeInDogYears= earlyYears + laterYears;
+//my name and age in dog years
+let MyName = "Enikardie".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
